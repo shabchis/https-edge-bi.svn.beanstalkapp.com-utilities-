63a48e753656006e2212da5eee515672b32ad4ab
alonyaari <alonyaari@7d275733-0047-4cae-8f02-c3d286dee25d>
2011-09-08 12:45:26 +0000

    Added Encrypt\Decrypt password button and made a few changes in Reset button

--- Edge.Applications.TempScheduler (2.9)/trunk/frmEncryptDecrypt.Designer.cs
+++ Edge.Applications.TempScheduler (2.9)/trunk/frmEncryptDecrypt.Designer.cs
@@ -28,44 +28,91 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(359, 249);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(194, 82);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(135, 20);
-			this.textBox1.TabIndex = 1;
-			// 
-			// frmEncryptDecrypt
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(563, 334);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.button1);
-			this.Name = "frmEncryptDecrypt";
-			this.Text = "frmEncryptDecrypt";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.pswdLabel = new System.Windows.Forms.Label();
+            this.encDecLabel = new System.Windows.Forms.Label();
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(205, 129);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.encryptBtn.TabIndex = 0;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Location = new System.Drawing.Point(205, 29);
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(220, 20);
+            this.inputTxt.TabIndex = 1;
+            // 
+            // pswdLabel
+            // 
+            this.pswdLabel.AutoSize = true;
+            this.pswdLabel.Location = new System.Drawing.Point(22, 36);
+            this.pswdLabel.Name = "pswdLabel";
+            this.pswdLabel.Size = new System.Drawing.Size(56, 13);
+            this.pswdLabel.TabIndex = 2;
+            this.pswdLabel.Text = "Password:";
+            // 
+            // encDecLabel
+            // 
+            this.encDecLabel.AutoSize = true;
+            this.encDecLabel.Location = new System.Drawing.Point(22, 84);
+            this.encDecLabel.Name = "encDecLabel";
+            this.encDecLabel.Size = new System.Drawing.Size(161, 13);
+            this.encDecLabel.TabIndex = 3;
+            this.encDecLabel.Text = "Encrypted\\Decrypted Password:";
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Location = new System.Drawing.Point(205, 81);
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.Size = new System.Drawing.Size(220, 20);
+            this.outputTxt.TabIndex = 4;
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(350, 129);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.decryptBtn.TabIndex = 5;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // frmEncryptDecrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 201);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.outputTxt);
+            this.Controls.Add(this.encDecLabel);
+            this.Controls.Add(this.pswdLabel);
+            this.Controls.Add(this.inputTxt);
+            this.Controls.Add(this.encryptBtn);
+            this.Name = "frmEncryptDecrypt";
+            this.Text = "frmEncryptDecrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button encryptBtn;
+		private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.Label pswdLabel;
+        private System.Windows.Forms.Label encDecLabel;
+        private System.Windows.Forms.TextBox outputTxt;
+        private System.Windows.Forms.Button decryptBtn;
 	}
 }
