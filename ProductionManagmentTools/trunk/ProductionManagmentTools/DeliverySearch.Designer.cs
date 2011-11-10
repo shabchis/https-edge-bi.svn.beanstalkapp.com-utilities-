4134f131a7aabe715ca2b4fbf4484b8719df78b8
sbarchen <sbarchen@7d275733-0047-4cae-8f02-c3d286dee25d>
2011-11-10 14:51:23 +0000

    Commited option for delivery search

--- ProductionManagmentTools/trunk/ProductionManagmentTools/DeliverySearch.Designer.cs
+++ ProductionManagmentTools/trunk/ProductionManagmentTools/DeliverySearch.Designer.cs
@@ -47,11 +47,6 @@
 			this.DeliveryID_tb = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.pictureBox4 = new System.Windows.Forms.PictureBox();
-			this.pictureBox5 = new System.Windows.Forms.PictureBox();
-			this.pictureBox6 = new System.Windows.Forms.PictureBox();
-			this.checkBox4 = new System.Windows.Forms.CheckBox();
 			this.checkBox2 = new System.Windows.Forms.CheckBox();
 			this.DeliveryDataGridView = new System.Windows.Forms.DataGridView();
 			this.DeliveryID = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -61,6 +56,7 @@
 			this.DateCreated = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.TargetPeriodStart = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.TargetPeriodEnd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Commited = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.label4 = new System.Windows.Forms.Label();
 			this.tabPage5 = new System.Windows.Forms.TabPage();
 			this.label12 = new System.Windows.Forms.Label();
@@ -125,10 +121,6 @@
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.groupBox1.SuspendLayout();
-			this.groupBox2.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.DeliveryDataGridView)).BeginInit();
 			this.tabPage5.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -163,9 +155,9 @@
 			this.dataTypes.Enabled = false;
 			this.dataTypes.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.dataTypes.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.dataTypes.Location = new System.Drawing.Point(7, 82);
+			this.dataTypes.Location = new System.Drawing.Point(7, 166);
 			this.dataTypes.Name = "dataTypes";
-			this.dataTypes.Size = new System.Drawing.Size(255, 90);
+			this.dataTypes.Size = new System.Drawing.Size(260, 90);
 			this.dataTypes.TabIndex = 57;
 			this.dataTypes.TabStop = false;
 			this.dataTypes.Text = "Data Types";
@@ -178,7 +170,7 @@
 			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
 			this.pictureBox3.Location = new System.Drawing.Point(8, 88);
 			this.pictureBox3.Name = "pictureBox3";
-			this.pictureBox3.Size = new System.Drawing.Size(0, 0);
+			this.pictureBox3.Size = new System.Drawing.Size(5, 0);
 			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox3.TabIndex = 48;
 			this.pictureBox3.TabStop = false;
@@ -191,7 +183,7 @@
 			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
 			this.pictureBox2.Location = new System.Drawing.Point(8, 65);
 			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(0, 0);
+			this.pictureBox2.Size = new System.Drawing.Size(5, 0);
 			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox2.TabIndex = 47;
 			this.pictureBox2.TabStop = false;
@@ -204,7 +196,7 @@
 			this.pictureBox1.Image = global::Edge.Application.ProductionManagmentTools.Properties.Resources.google;
 			this.pictureBox1.Location = new System.Drawing.Point(8, 43);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(0, 0);
+			this.pictureBox1.Size = new System.Drawing.Size(5, 0);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 46;
 			this.pictureBox1.TabStop = false;
@@ -252,7 +244,7 @@
 			this.label1.BackColor = System.Drawing.Color.Transparent;
 			this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
 			this.label1.ForeColor = System.Drawing.Color.Transparent;
-			this.label1.Location = new System.Drawing.Point(10, 6);
+			this.label1.Location = new System.Drawing.Point(10, 12);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(69, 14);
 			this.label1.TabIndex = 59;
@@ -267,9 +259,9 @@
 			this.groupBox1.Controls.Add(this.label3);
 			this.groupBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
 			this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.groupBox1.Location = new System.Drawing.Point(7, 188);
+			this.groupBox1.Location = new System.Drawing.Point(7, 279);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(255, 88);
+			this.groupBox1.Size = new System.Drawing.Size(260, 88);
 			this.groupBox1.TabIndex = 56;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Time period";
@@ -314,16 +306,16 @@
 			// 
 			// accountID
 			// 
-			this.accountID.Location = new System.Drawing.Point(85, 3);
+			this.accountID.Location = new System.Drawing.Point(13, 29);
 			this.accountID.Name = "accountID";
 			this.accountID.Size = new System.Drawing.Size(100, 20);
 			this.accountID.TabIndex = 60;
 			// 
 			// DeliveryID_tb
 			// 
-			this.DeliveryID_tb.Location = new System.Drawing.Point(85, 42);
+			this.DeliveryID_tb.Location = new System.Drawing.Point(13, 108);
 			this.DeliveryID_tb.Name = "DeliveryID_tb";
-			this.DeliveryID_tb.Size = new System.Drawing.Size(177, 20);
+			this.DeliveryID_tb.Size = new System.Drawing.Size(249, 20);
 			this.DeliveryID_tb.TabIndex = 62;
 			// 
 			// label2
@@ -332,7 +324,7 @@
 			this.label2.BackColor = System.Drawing.Color.Transparent;
 			this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
 			this.label2.ForeColor = System.Drawing.Color.Transparent;
-			this.label2.Location = new System.Drawing.Point(10, 45);
+			this.label2.Location = new System.Drawing.Point(10, 91);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(61, 14);
 			this.label2.TabIndex = 61;
@@ -349,90 +341,21 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// groupBox2
-			// 
-			this.groupBox2.BackColor = System.Drawing.Color.Transparent;
-			this.groupBox2.Controls.Add(this.pictureBox4);
-			this.groupBox2.Controls.Add(this.pictureBox5);
-			this.groupBox2.Controls.Add(this.pictureBox6);
-			this.groupBox2.Controls.Add(this.checkBox4);
-			this.groupBox2.Controls.Add(this.checkBox2);
-			this.groupBox2.Enabled = false;
-			this.groupBox2.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.groupBox2.Location = new System.Drawing.Point(7, 287);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(255, 90);
-			this.groupBox2.TabIndex = 58;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Advanced Search";
-			// 
-			// pictureBox4
-			// 
-			this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-			this.pictureBox4.Location = new System.Drawing.Point(8, 88);
-			this.pictureBox4.Name = "pictureBox4";
-			this.pictureBox4.Size = new System.Drawing.Size(0, 0);
-			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox4.TabIndex = 48;
-			this.pictureBox4.TabStop = false;
-			// 
-			// pictureBox5
-			// 
-			this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-			this.pictureBox5.Location = new System.Drawing.Point(8, 65);
-			this.pictureBox5.Name = "pictureBox5";
-			this.pictureBox5.Size = new System.Drawing.Size(0, 0);
-			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox5.TabIndex = 47;
-			this.pictureBox5.TabStop = false;
-			// 
-			// pictureBox6
-			// 
-			this.pictureBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBox6.Image = global::Edge.Application.ProductionManagmentTools.Properties.Resources.google;
-			this.pictureBox6.Location = new System.Drawing.Point(8, 43);
-			this.pictureBox6.Name = "pictureBox6";
-			this.pictureBox6.Size = new System.Drawing.Size(0, 0);
-			this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox6.TabIndex = 46;
-			this.pictureBox6.TabStop = false;
-			// 
-			// checkBox4
-			// 
-			this.checkBox4.AutoSize = true;
-			this.checkBox4.Checked = true;
-			this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.checkBox4.Font = new System.Drawing.Font("Microsoft JhengHei", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.checkBox4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-			this.checkBox4.Location = new System.Drawing.Point(20, 58);
-			this.checkBox4.Name = "checkBox4";
-			this.checkBox4.Size = new System.Drawing.Size(72, 19);
-			this.checkBox4.TabIndex = 40;
-			this.checkBox4.Text = "With files";
-			this.checkBox4.UseVisualStyleBackColor = true;
-			// 
 			// checkBox2
 			// 
 			this.checkBox2.AutoSize = true;
 			this.checkBox2.Checked = true;
 			this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.checkBox2.Font = new System.Drawing.Font("Microsoft JhengHei", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
 			this.checkBox2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-			this.checkBox2.Location = new System.Drawing.Point(20, 33);
+			this.checkBox2.Location = new System.Drawing.Point(13, 62);
 			this.checkBox2.Name = "checkBox2";
-			this.checkBox2.Size = new System.Drawing.Size(76, 19);
+			this.checkBox2.Size = new System.Drawing.Size(180, 19);
 			this.checkBox2.TabIndex = 40;
-			this.checkBox2.Text = "Commited";
+			this.checkBox2.Text = "Show only Commited Deliveries";
 			this.checkBox2.UseVisualStyleBackColor = true;
+			this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
 			// 
 			// DeliveryDataGridView
 			// 
@@ -455,7 +378,8 @@
             this.Channel_ID,
             this.DateCreated,
             this.TargetPeriodStart,
-            this.TargetPeriodEnd});
+            this.TargetPeriodEnd,
+            this.Commited});
 			this.DeliveryDataGridView.Location = new System.Drawing.Point(320, 26);
 			this.DeliveryDataGridView.Name = "DeliveryDataGridView";
 			this.DeliveryDataGridView.Size = new System.Drawing.Size(821, 218);
@@ -497,6 +421,11 @@
 			// 
 			this.TargetPeriodEnd.HeaderText = "TargetPeriodEnd";
 			this.TargetPeriodEnd.Name = "TargetPeriodEnd";
+			// 
+			// Commited
+			// 
+			this.Commited.HeaderText = "Commited";
+			this.Commited.Name = "Commited";
 			// 
 			// label4
 			// 
@@ -832,7 +761,7 @@
 			this.DeliveryHistoryParams.ShowCellToolTips = false;
 			this.DeliveryHistoryParams.ShowEditingIcon = false;
 			this.DeliveryHistoryParams.ShowRowErrors = false;
-			this.DeliveryHistoryParams.Size = new System.Drawing.Size(292, 178);
+			this.DeliveryHistoryParams.Size = new System.Drawing.Size(396, 178);
 			this.DeliveryHistoryParams.TabIndex = 77;
 			// 
 			// dataGridViewTextBoxColumn1
@@ -1100,9 +1029,9 @@
 			// 
 			this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
 			this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-			this.pictureBox7.Location = new System.Drawing.Point(53, 448);
+			this.pictureBox7.Location = new System.Drawing.Point(66, 521);
 			this.pictureBox7.Name = "pictureBox7";
-			this.pictureBox7.Size = new System.Drawing.Size(145, 125);
+			this.pictureBox7.Size = new System.Drawing.Size(140, 126);
 			this.pictureBox7.TabIndex = 87;
 			this.pictureBox7.TabStop = false;
 			// 
@@ -1114,9 +1043,9 @@
 			this.panel1.Controls.Add(this.groupBox1);
 			this.panel1.Controls.Add(this.pictureBox7);
 			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.checkBox2);
 			this.panel1.Controls.Add(this.label2);
 			this.panel1.Controls.Add(this.dataTypes);
-			this.panel1.Controls.Add(this.groupBox2);
 			this.panel1.Controls.Add(this.DeliveryID_tb);
 			this.panel1.Controls.Add(this.button1);
 			this.panel1.Location = new System.Drawing.Point(12, 12);
@@ -1136,6 +1065,7 @@
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.DeliveryDataGridView);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Name = "DeliverySearch";
 			this.ShowIcon = false;
 			this.Text = "DeliverySearch";
 			this.dataTypes.ResumeLayout(false);
@@ -1145,11 +1075,6 @@
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
-			this.groupBox2.ResumeLayout(false);
-			this.groupBox2.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.DeliveryDataGridView)).EndInit();
 			this.tabPage5.ResumeLayout(false);
 			this.tabPage5.PerformLayout();
@@ -1198,14 +1123,9 @@
         private System.Windows.Forms.TextBox accountID;
         private System.Windows.Forms.TextBox DeliveryID_tb;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.DataGridView DeliveryDataGridView;
-        private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.DataGridView DeliveryDataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn DeliveryID;
         private System.Windows.Forms.DataGridViewTextBoxColumn Account_ID;
         private System.Windows.Forms.DataGridViewTextBoxColumn OriginalID;
@@ -1272,5 +1192,6 @@
 		private System.Windows.Forms.PictureBox pictureBox8;
 		private System.Windows.Forms.Label label14;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Commited;
     }
 }
