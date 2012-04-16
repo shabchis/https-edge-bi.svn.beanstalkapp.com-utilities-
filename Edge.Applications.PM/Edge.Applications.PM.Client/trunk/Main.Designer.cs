9143215a6ccd8a7bcdd0e13ce31f2e3dbf35706e
sbarchen <sbarchen@7d275733-0047-4cae-8f02-c3d286dee25d>
2012-04-16 11:02:27 +0000

    wokring version with tools ref

--- Edge.Applications.PM/Edge.Applications.PM.Client/trunk/Main.Designer.cs
+++ Edge.Applications.PM/Edge.Applications.PM.Client/trunk/Main.Designer.cs
@@ -31,7 +31,7 @@
 			this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
 			this.menuStrip = new System.Windows.Forms.MenuStrip();
 			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+			this.toolStripContainer1.ContentPanel.SuspendLayout();
 			this.toolStripContainer1.SuspendLayout();
 			this.menuStrip.SuspendLayout();
 			this.SuspendLayout();
@@ -42,35 +42,33 @@
 			// 
 			// toolStripContainer1.ContentPanel
 			// 
-			this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(967, 464);
+			this.toolStripContainer1.ContentPanel.Controls.Add(this.menuStrip);
+			this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(967, 49);
 			this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.toolStripContainer1.LeftToolStripPanelVisible = false;
 			this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
 			this.toolStripContainer1.Name = "toolStripContainer1";
 			this.toolStripContainer1.RightToolStripPanelVisible = false;
-			this.toolStripContainer1.Size = new System.Drawing.Size(967, 488);
+			this.toolStripContainer1.Size = new System.Drawing.Size(967, 49);
 			this.toolStripContainer1.TabIndex = 0;
 			this.toolStripContainer1.Text = "toolStripContainer1";
 			// 
-			// toolStripContainer1.TopToolStripPanel
-			// 
-			this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip);
-			// 
 			// menuStrip
 			// 
+			this.menuStrip.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.menuStrip.ImeMode = System.Windows.Forms.ImeMode.On;
 			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1});
 			this.menuStrip.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip.Name = "menuStrip";
-			this.menuStrip.Size = new System.Drawing.Size(967, 24);
+			this.menuStrip.Size = new System.Drawing.Size(967, 49);
 			this.menuStrip.TabIndex = 0;
 			this.menuStrip.Text = "menuStrip1";
 			// 
 			// toolStripMenuItem1
 			// 
 			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-			this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 45);
 			this.toolStripMenuItem1.Text = "9999";
 			this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
 			// 
@@ -79,13 +77,13 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.ClientSize = new System.Drawing.Size(967, 488);
+			this.ClientSize = new System.Drawing.Size(967, 49);
 			this.Controls.Add(this.toolStripContainer1);
 			this.MainMenuStrip = this.menuStrip;
 			this.Name = "Main";
 			this.Text = "Form1";
-			this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
-			this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+			this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+			this.toolStripContainer1.ContentPanel.PerformLayout();
 			this.toolStripContainer1.ResumeLayout(false);
 			this.toolStripContainer1.PerformLayout();
 			this.menuStrip.ResumeLayout(false);
