551694575d4b8eb8360db686047b02501f91cafa
sbarchen <sbarchen@7d275733-0047-4cae-8f02-c3d286dee25d>
2012-07-30 07:52:34 +0000

    Enum Project. Not finished: CompareWithExistingValues function

---
+++ Edge.Utilities.CLR.EdgeEnumObjects/EnumTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edge.Utilities.CLR.EdgeEnumObjects
+{
+	public class EnumTypes
+	{
+		public enum ObjectStatus
+		{
+			Unknown = 0,
+			Active = 1,
+			Paused = 2,
+			Suspended = 3,
+			Ended = 4,
+			Deleted = 5,
+			Pending = 6
+		}
+		public enum TextCreativeType
+		{
+			Title = 1,
+			Body = 2,
+			DisplayUrl = 3
+		}
+	}
+}
