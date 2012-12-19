3de10427ef4a0726f658def65223459b2f06bc5e
shirat <shirat@7d275733-0047-4cae-8f02-c3d286dee25d>
2012-12-19 07:38:04 +0000

    Add Converters directory to Scheduler UI

---
+++ Edge.Applications.PM.Suite.Scheduler/trunk/Converters/Outcome2ImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using Edge.Core.Services;
+using System;
+using System.Windows.Data;
+
+namespace Edge.Applications.PM.SchedulerControl.Converters
+{
+	public class Outcome2ImageConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var outcome = (ServiceOutcome)value;
+
+			var iconPath = outcome == ServiceOutcome.Failure || outcome == ServiceOutcome.Success
+				           ? String.Format("..\\Icons\\{0}.ico", outcome.ToString())
+				           : null;
+
+			return iconPath;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
