902b37c8dc2b4ccb5426880e269f8e17e9dc06f8
sbarchen <sbarchen@7d275733-0047-4cae-8f02-c3d286dee25d>
2013-01-01 13:58:05 +0000

    CPA Alerts Added to SVN

---
+++ Edge.Utilities.CLR.Alerts.CampaignCPA/Edge.Utilities.CLR.Alerts.CampaignCPA/Test Scripts/Test.sql
@@ -0,0 +1,19 @@
+﻿DECLARE	@return_value int,
+		@returnMsg nvarchar(4000)
+
+EXEC	@return_value = [dbo].[CLR_Alerts_ConversionAnalysis]
+		@AccountID = 7,
+		@Period = 30,
+		@ToDay = N'2012-06-02 00:00:00',
+		@ChannelID = NULL,
+		@threshold = 2,
+		@excludeIds = '4004457,4002683,4002762,4004836',
+		@cubeName = N'BOEasyForex2',
+		@acqFieldName = N'Actives',
+		@cpaFieldName = N'Cost/Active',
+		@returnMsg = @returnMsg OUTPUT
+
+SELECT	@returnMsg as N'@returnMsg'
+
+SELECT	'Return Value' = @return_value
+
