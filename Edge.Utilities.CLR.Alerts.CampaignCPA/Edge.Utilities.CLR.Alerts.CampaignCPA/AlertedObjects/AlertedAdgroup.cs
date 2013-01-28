20ff367e74983e130142295f93ec74d9c3b70a2b
sbarchen <sbarchen@7d275733-0047-4cae-8f02-c3d286dee25d>
2013-01-28 14:40:34 +0000

    Added Priority and "Order BY " !!!! not working yet !!!!!

--- Edge.Utilities.CLR.Alerts.CampaignCPA/Edge.Utilities.CLR.Alerts.CampaignCPA/AlertedObjects/AlertedAdgroup.cs
+++ Edge.Utilities.CLR.Alerts.CampaignCPA/Edge.Utilities.CLR.Alerts.CampaignCPA/AlertedObjects/AlertedAdgroup.cs
@@ -7,9 +7,13 @@ namespace Edge.Utilities.CLR.Alerts.CampaignCPA.AlertedObjects
 {
 	public class AlertedAdgroup : AbstractAlertedUnit
 	{
+		public double Priority = 1;
+		public string CampaignName = string.Empty;
+
 		public AlertedAdgroup(System.Data.SqlClient.SqlDataReader reader, string extraFields, string acq1FieldName, string acq2FieldName, string cpaFieldName, string cprFieldName)
 		{
-			this.Name =  Convert.ToString(reader["[Getways Dim].[Gateways].[Ad Group].[MEMBER_CAPTION]"]);
+			this.Name = Convert.ToString(reader["[Getways Dim].[Gateways].[Ad Group].[MEMBER_CAPTION]"]);
+			CampaignName = Convert.ToString(reader["[Getways Dim].[Gateways].[Campaign].[MEMBER_CAPTION]"]);
 
 			Dictionary<string, object> ExtraFields = new Dictionary<string, object>();
 
@@ -18,6 +22,9 @@ namespace Edge.Utilities.CLR.Alerts.CampaignCPA.AlertedObjects
 			this.Acq2 = reader["[Measures].[" + acq2FieldName + "]"] == DBNull.Value ? 0 : Convert.ToDouble(reader["[Measures].[" + acq2FieldName + "]"]);
 			this.CPR = reader["[Measures].[" + cprFieldName + "]"] == DBNull.Value ? 0 : Convert.ToDouble(reader["[Measures].[" + cprFieldName + "]"]);
 			this.CPA = reader["[Measures].[" + cpaFieldName + "]"] == DBNull.Value ? 0 : Convert.ToDouble(reader["[Measures].[" + cpaFieldName + "]"]);
+
+			if (this.Cost != 0)
+				this.Priority = (this.Acq2 * 1000 + this.Acq1 * 100) / this.Cost;
 
 			if (!string.IsNullOrEmpty(extraFields))
 			{
