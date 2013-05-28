c7f80d87095c67d8ee3ec4f9d3edd2d0b1f8e8c4
sbarchen <sbarchen@7d275733-0047-4cae-8f02-c3d286dee25d>
2013-05-28 13:13:02 +0000

    Added Account History

--- Edge.Utilities.AdwordsAPI.ReportsTool/Form1.Designer.cs
+++ Edge.Utilities.AdwordsAPI.ReportsTool/Form1.Designer.cs
@@ -56,6 +56,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.webExceptionTextBox = new System.Windows.Forms.RichTextBox();
             this.get_Acc_Lables = new System.Windows.Forms.Button();
+            this.GetAccountHistory = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -305,11 +306,22 @@
             this.get_Acc_Lables.UseVisualStyleBackColor = true;
             this.get_Acc_Lables.Click += new System.EventHandler(this.get_Acc_Lables_Click);
             // 
+            // GetAccountHistory
+            // 
+            this.GetAccountHistory.Location = new System.Drawing.Point(1246, 78);
+            this.GetAccountHistory.Name = "GetAccountHistory";
+            this.GetAccountHistory.Size = new System.Drawing.Size(111, 23);
+            this.GetAccountHistory.TabIndex = 33;
+            this.GetAccountHistory.Text = "Get account History";
+            this.GetAccountHistory.UseVisualStyleBackColor = true;
+            this.GetAccountHistory.Click += new System.EventHandler(this.GetAccountHistory_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1393, 839);
+            this.Controls.Add(this.GetAccountHistory);
             this.Controls.Add(this.get_Acc_Lables);
             this.Controls.Add(this.webExceptionTextBox);
             this.Controls.Add(this.textBox1);
@@ -377,6 +389,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox webExceptionTextBox;
         private System.Windows.Forms.Button get_Acc_Lables;
+        private System.Windows.Forms.Button GetAccountHistory;
     }
 }
 
